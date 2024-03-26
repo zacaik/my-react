c046e74603e184ec2c0229da21cddbe53a6f1368
lijunran <646319370@qq.com>
2024-03-26 22:06:23 +0800

    feat: 项目工程搭建

--- packages/my-app1/index.js
+++ packages/my-app1/index.js
@@ -2,8 +2,7 @@
  * @Author: lijunran 646319370@qq.com
  * @Date: 2024-03-21 00:52:05
  * @LastEditors: lijunran 646319370@qq.com
- * @LastEditTime: 2024-03-21 00:52:11
+ * @LastEditTime: 2024-03-26 21:53:57
  * @FilePath: \my-react\packages\my-app1\index.js
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-console.log("a");
+console.log('a');
